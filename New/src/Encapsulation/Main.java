def6aafa38f21eac5cfa6f6eddfcdc2d06b5ab4b
x230 <x230@192.168.1.40>
2025-12-10 11:30:52 +0530

    Encapsulation Package Added under New Project

---
+++ New/src/Encapsulation/Main.java
@@ -0,0 +1,15 @@
+package Encapsulation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student obj = new Student();
+		obj.setName("Ashok");
+		
+		System.out.println(obj.getName());
+
+	}
+
+}
